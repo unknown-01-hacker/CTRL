ab403fcaa407dcee66ce3ca34d7c70687686eb54
noobanon <51384033+noobanon@users.noreply.github.com>
2020-03-05 02:10:58 +0530

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -24,7 +24,7 @@ from tg_bot.modules.connection import connected
 
 HANDLER_GROUP = 10
 
-
+VALID_WELCOME_FORMATTERS = ['first', 'last', 'fullname', 'username', 'id', 'count', 'chatname', 'mention']
 
 def escape_html(word):
     return escape(word)
@@ -196,7 +196,7 @@ def reply_filter(bot: Bot, update: Update):
         return
 
     chat_filters = sql.get_chat_triggers(chat.id)
-    for keyword in chat_filters:
+    for keyword in chat_filters
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
         if re.search(pattern, to_match, flags=re.IGNORECASE):
             filt = sql.get_filter(chat.id, keyword)
@@ -218,7 +218,7 @@ def reply_filter(bot: Bot, update: Update):
                 keyboard = InlineKeyboardMarkup(keyb)
 
                 try:
-                    message.reply_text(filt.reply, parse_mode=ParseMode.MARKDOWN,
+                    message.reply_text(filt.reply,.format(first=first_name) parse_mode=ParseMode.MARKDOWN,
                                        disable_web_page_preview=True,
                                        reply_markup=keyboard)
                 except BadRequest as excp:
